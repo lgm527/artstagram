b6e0b07420c34448f09846f9eb8aedb09ec22a23
lgm527 <laurellmccaffrey@gmail.com>
2019-08-20 14:32:22 -0400

    started it, yay

---
+++ artstagram-backend/app/models/picture.rb
@@ -0,0 +1,4 @@
+class Picture < ApplicationRecord
+  has_many :comments
+  has_many :likes
+end
---
+++ artstagram-backend/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :likes
+  resources :comments
+  resources :pictures
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
