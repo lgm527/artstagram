ad66f38084e8d655699c9742fd58320d112144e9
lgm527 <laurellmccaffrey@gmail.com>
2019-08-21 15:10:30 -0400

    comments count works now too

--- artstagram-backend/app/controllers/comments_controller.rb
+++ artstagram-backend/app/controllers/comments_controller.rb
@@ -2,16 +2,16 @@ class CommentsController < ApplicationController
 
   def index
     @comments = Comment.all
-    @likes = Like.all
     render json: @comments
   end
 
   def create
-    @comment = Comment.new
+    @comment = Comment.create(comment_params)
   end
 
   def show
-
+    @comment = Comment.find(params[:id])
+    render json: @comment
   end
 
   def update
@@ -20,6 +20,12 @@ class CommentsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :picture_id)
   end
 
 end
--- artstagram-backend/app/controllers/pictures_controller.rb
+++ artstagram-backend/app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
 
   def index
     @pictures = Picture.all
-    render json: @pictures, include: [:likes]
+    render json: @pictures, include: [:likes, :comments]
   end
 
 end
