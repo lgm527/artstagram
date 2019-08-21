a911f226d5536d35d2ae1d95e22e5def26aeb32d
lgm527 <laurellmccaffrey@gmail.com>
2019-08-21 14:23:51 -0400

    like functionality complete

--- artstagram-frontend/main.js
+++ artstagram-frontend/main.js
@@ -3,16 +3,11 @@ document.addEventListener('DOMContentLoaded', function(){
 const picContainer = document.getElementsByClassName('ui three cards')[0];
 
   //fetch pics from backend
-  // fetch('http://localhost:3000/likes')
-  // .then(res => res.json())
-  // .then(renderLikes)
-
-  //fetch likes from backend
   fetch('http://localhost:3000/pictures')
   .then(res => res.json())
   .then(renderPics)
 
-  //create and fill in html with json
+  //create and render a picture on DOM
   function renderPic(pic){
     let picDiv = document.createElement('div')
     picDiv.className = 'ui card'
@@ -21,8 +16,8 @@ const picContainer = document.getElementsByClassName('ui three cards')[0];
                         </div>
                         <div class="content">
                             <span class="right floated">
-                            <i class="heart outline like icon"></i>
-                            <!-- insert likes here -->${pic.likes.length} likes
+                            <i class="heart outline like icon" data-id="${pic.id}"></i>
+                            <span id='likes' data-id="${pic.id}"> ${pic.likes.length} </span>
                             </span>
                         <i class="comment icon"></i>
                         <!-- insert likes here --> comments
@@ -30,11 +25,30 @@ const picContainer = document.getElementsByClassName('ui three cards')[0];
       picContainer.append(picDiv);
   }
 
-
+  //render all pictures
   function renderPics(pics){
     pics.forEach(renderPic)
   }
 
+  //add like feature to stable parent:
+  picContainer.addEventListener('click', function(event){
+    if (event.target.classList.contains("heart")){
+      let picId = event.target.dataset.id;
+
+      fetch("http://localhost:3000/likes",{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'picture_id': Number(picId)
+        })
+      })
+      .then(res => res.json())
+      .then(data => data)
+      event.target.nextElementSibling.innerText++;
+    }
+  })
 
 
 
